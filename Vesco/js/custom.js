dad47e3d2d6f58e3fab4ad1beb96f519a029cfb3
Josh Samuel O. Monreal <mojs@erni.ph>
2018-10-10 14:25:15 +0800

    Initial Commit

---
+++ Vesco/js/custom.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+    new WOW().init();
+
+    $("#work").magnificPopup({ 
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
