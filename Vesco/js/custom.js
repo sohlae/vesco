e5675d271098b507289b5755f8353a5d3b2a9278
Josh Samuel O. Monreal <mojs@erni.ph>
2018-10-15 19:16:47 +0800

    Added custom code for initializing the owl-carousel library.

--- Vesco/js/custom.js
+++ Vesco/js/custom.js
@@ -1,11 +1,19 @@
-﻿$(function() {
+﻿$(function () {
     new WOW().init();
 
-    $("#work").magnificPopup({ 
+    $("#work").magnificPopup({
         delegate: 'a',
         type: 'image',
         gallery: {
             enabled: true
         }
+    });
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
     });
 });
